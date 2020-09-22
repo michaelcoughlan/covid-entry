1a6b79a6c63aea6efaf44440efe60312d0cd7574
Michael Coughlan <micoughlan@deloitte.ie>
2020-09-22 15:04:55 +0100

    Update build command

--- bin/build-fe.sh
+++
@@ -1,9 +0,0 @@
-# Remove the previous public folder in the server
-rm -rf ../server/public
-
-# Change directory to the client and run the script for the production build
-cd ../client
-npm run build
-
-# Move the production build to the server side
-mv build ../server/public
---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+# Remove the previous public folder in the server
+rm -rf ../server/public
+
+# Change directory to the client and run the build command
+cd ../client
+npm run build
+
+# Move the production build to the server side
+mv build ../server/public
+
+# Change directory to the server and run the build command
+cd ../server
+npm run build
